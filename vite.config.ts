561faca2d418e096d39238d6a6439c9122acb483
Keerthika <keer05thi07ka-alt@github.com>
2026-09-10 22:00:43 +0530

    Update vite config for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,11 @@ export default defineConfig({
   plugins: [
     tsConfigPaths({ projects: ["./tsconfig.json"] }),
     tailwindcss(),
-    tanstackStart(),
+    tanstackStart({
+      server: {
+        preset: 'vercel'
+      }
+    }),
     viteReact(),
   ],
 });
