c42e687e7b56d7ca5d53b4e8c6ea969602920427
Keerthika <keer05thi07ka-alt@github.com>
2026-09-13 21:42:35 +0530

    feat: Transform app into an installable Offline PWA using vite-plugin-pwa

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import tailwindcss from "@tailwindcss/vite";
 import viteReact from "@vitejs/plugin-react";
 
+import { VitePWA } from 'vite-plugin-pwa';
+
 export default defineConfig({
   server: {
     port: 3000,
@@ -18,5 +20,25 @@ export default defineConfig({
       }
     }),
     viteReact(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: { enabled: true }, // Enable in dev mode for hackathon demo
+      manifest: {
+        name: 'Aarogya Health',
+        short_name: 'Aarogya',
+        theme_color: '#ffffff',
+        display: "standalone",
+        icons: [
+          {
+            src: 'https://cdn-icons-png.flaticon.com/512/3063/3063206.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,json,woff,woff2}']
+      }
+    }),
   ],
 });
